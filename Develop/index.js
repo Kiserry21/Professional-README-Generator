ceaba252cc0fcf9e0b321c883be67421465335e0
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2022-11-08 20:57:33 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,124 @@ const fs = require("fs");
 const util = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "What is your project's description?",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTable',
+        message: 'Do you want a Table of Contents?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are your installation instructions?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter any info on how to install!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your usage information?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter any info for usage!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What License applies to your application?',
+        choices: ['GNU LGPL', 'Mozilla', 'Apache', 'MIT', 'Boost Software', 'The unlicense']        
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: "What are your contribution guidelines?",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter any how to contribute info!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are your test instructions?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter any how to test info!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: "What is your GitHub Username?",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a email!');
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
