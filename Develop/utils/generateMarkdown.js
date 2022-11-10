75d8fc20e0959ca3d29bcf1d0374576ea6cdc50b
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2022-11-09 22:07:28 -0500

    created function for license badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,14 @@
+const licensesDetails = require('./licensesDetails.js')
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == "None") {
+    return ``;
+  }
+  let result = licensesDetails.filter(licenseDetail => licenseDetail.name == license);
+  return result[0].badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
